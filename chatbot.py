02599b0a5c233454881da0fa67efdee033bb52d2
Brooklyn <58751447+bbrooklyn@users.noreply.github.com>
2023-11-27 18:16:30 +0000

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -23,13 +23,6 @@ class App(customtkinter.CTk):
         self.scrollable_frame = customtkinter.CTkScrollableFrame(self, width=200, height=200)
         self.scrollable_frame.grid(row=1, column=0, columnspan=2, padx=5, pady=5, sticky="new")
         
-        
-    def new_bubble(self, message, sender):
-        # Create a new bubble
-        bubble = customtkinter.CTkBubble(self, text=message, sender=sender)
-        # Add the bubble to the chat
-        self.chat.add_bubble(bubble
-        )
     def send_response(self, response):
         print(response)
         
